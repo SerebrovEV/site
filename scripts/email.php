a8fdda8c66342b4624fc7728d6cb955a58b15a75
SerebrovEV <89818121788@mail.ru>
2023-08-17 17:01:34 +0300

    Progress at 17/08 part 2

--- scripts/email.php
+++ scripts/email.php
@@ -1,44 +1,54 @@
 <?php
-$to = "89818121788@mail.ru";//Почтовый ящик на который будет отправленно сообщение
+  $to = "89818121788@mail.ru";//Почтовый ящик на который будет отправлено сообщение
   $subject = "Тема сообщения";//Тема сообщения
   $message = "Message, сообщение!";//Сообщение, письмо
   $headers = "Content-type: text/plain; charset=utf-8 \r\n";
   //Шапка сообщения, содержит определение типа письма, от кого, и кому отправить ответ на письмо
+
 // Проверяем или метод запроса POST
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 		// Поочередно проверяем или были переданные параметры формы, или они не пустые
-		if(isset($_POST["user-name"]){
+		if(isset($_POST['name'])){
 			//Если параметр есть, присваеваем ему переданое значение
-			$name 		=trim(strip_tags($_POST["user-name"]));
+			$name =trim(strip_tags($_POST['name']));
 		}
-		if(isset($_POST["user-phone"]))
+
+		if(isset($_POST['phone']))
 		{
-			$number 	= trim(strip_tags($_POST["user-phone"]));
+			$number=trim(strip_tags($_POST['phone']));
 		}
-		if (isset( $_POST["user-work"])) {
-        			$type 	= trim(strip_tags($type));
-        		}
-		if (isset( $_POST["user-info"])) {
-			$question 	= trim(strip_tags($question));
+
+		if (isset( $_POST['type'])) {
+        	$type=trim(strip_tags($_POST['type']));
+        }
+
+		if (isset( $_POST['description'])) {
+			$question=trim(strip_tags($_POST['description']));
 		}
+
 			// Формируем письмо
-			$message  = "<html>";
-				$message  .= "<body>";
+			$message = "Заявка на расчет";
 				$message  .= "Телефон: ".$number;
-				$message  .= "<br />";
+				$message  .= "\n";
 				$message  .= "Имя: ".$name;
-				$message  .= "<br />";
+				$message  .= "\n";
 				$message  .= "Тип работ: ".$type;
-                $message  .= "<br />";
-				$message  .= "Вопрос: ".$question;
-				$message  .= "</body>";
-			$message  .= "</html>";
+                $message  .= "<\n>";
+				$message  .= "Подробнее: ".$question;
+				$message  .= "<\n>";
 			// Окончание формирования тела письма
+
 			// Посылаем письмо
-			mail ($to, $subject, $message, $headers);
+			$send = mail ($to, $subject, $message, $headers);
+
+			if ($send) //проверяем, отправилось ли сообщение
+            		echo "Сообщение отправлено успешно! Перейти на <a href='https://etp-pro.ru</a>, если вас не перенаправило вручную.";
+            	else
+            		echo "Ошибка, сообщение не отправлено! Возможно, проблемы на сервере";
 }
+
 else
-{
+    {
 	exit;
-} 
+    }
 ?>
